237a22eb9fa961dcd7954abd4c42351aed54e4f5
Genna Walsh <gennawalsh@gmail.com>
2018-09-11 15:18:30 +0100

    completed int to month function

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -38,5 +38,23 @@ def number_to_full_month_name( i )
     return "February"
   when 3
     return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
   end
 end
